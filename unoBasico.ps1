9c5999afbea0185431b6e5b187253c6184a43180
dam1 - López López <A22LauraLL@sanclemente.local>
2023-10-31 10:56:08 +0100

    3 archivos del otro día

---
+++ unoBasico.ps1
@@ -0,0 +1,32 @@
+﻿#Comentario de una línea
+
+<#
+Comentario
+de
+lineas
+#>
+
+Write-Host "Este es nuestro segundo programa"
+
+$numero = 1
+#Empleamos el . como separador
+$decimal = 1.1
+
+Write-Host $numero.GetType().Name
+
+#No es case sensitive
+Write-Host $NuMeRo.GetType().Name
+Write-Host $decimal.GetType().Name
+
+$nombre = "Laura"
+$apellido = "López"
+
+$nombreCompleto = $nombre + ' ' + $apellido
+Write-Host "Tu nombre completo es: " $nombreCompleto
+
+[String]$numeroFalso = 5
+Write-Host "Tipo de la variable numeroFalso: " $numeroFalso.GetType().Name
+$otroNumero = 5
+$x = $numeroFalso + $otroNumero
+
+Write-Host "Suma: " $x
